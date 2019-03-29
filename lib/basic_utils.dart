11075d530e157b8c84a5f22d7b164bafb39c301c
Ephenodrom <daniel@ephenodrom.de>
2019-03-29 09:14:48 +0100

    update

--- lib/basic_utils.dart
+++ lib/basic_utils.dart
@@ -1,12 +1,15 @@
 library basic_utils;
 
 import 'dart:convert';
+import 'dart:math';
 
 import "src/model/CountryCodeList.dart";
 import "src/model/Domain.dart";
 import "src/model/GtldList.dart";
 import "src/model/IdnCountryCodeList.dart";
 import "src/model/PublicSuffix.dart";
+import "src/model/LengthUnits.dart";
 
 part "src/DomainUtils.dart";
 part "src/StringUtils.dart";
+part "src/MathUtils.dart";
---
+++ test/math_utils_test.dart
@@ -0,0 +1,44 @@
+import 'package:basic_utils/basic_utils.dart';
+import "package:test/test.dart";
+
+void main() {
+  test('Test defaultString', () {
+    expect(StringUtils.defaultString("Hello", "World"), "Hello");
+    expect(StringUtils.defaultString(null, "World"), "World");
+  });
+
+  test('Test camelCaseToUpperUnderscore', () {
+    expect(StringUtils.camelCaseToUpperUnderscore("camelCase"), "CAMEL_CASE");
+  });
+
+  test('Test camelCaseToLowerUnderscore', () {
+    expect(StringUtils.camelCaseToLowerUnderscore("camelCase"), "camel_case");
+  });
+
+  test('Test isLowerCase', () {
+    expect(StringUtils.isLowerCase("c"), true);
+    expect(StringUtils.isLowerCase("C"), false);
+  });
+
+  test('Test isUpperCase', () {
+    expect(StringUtils.isUpperCase("C"), true);
+    expect(StringUtils.isUpperCase("c"), false);
+  });
+
+  test('Test isNullOrEmpty', () {
+    expect(StringUtils.isNullOrEmpty(""), true);
+    expect(StringUtils.isNullOrEmpty(null), true);
+    expect(StringUtils.isNullOrEmpty("Hello"), false);
+  });
+
+  test('Test isNotNullOrEmpty', () {
+    expect(StringUtils.isNotNullOrEmpty(""), false);
+    expect(StringUtils.isNotNullOrEmpty(null), false);
+    expect(StringUtils.isNotNullOrEmpty("Hello"), true);
+  });
+
+  test('Test isAscii', () {
+    expect(StringUtils.isAscii("I am pure ascii"), true);
+    expect(StringUtils.isAscii("I am nö ascii"), false);
+  });
+}
