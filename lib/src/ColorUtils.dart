0e2fcac80eb28af3c58a5295cd72956437e47b8b
Anthony Whitford <anthony@whitford.com>
2020-10-03 19:55:54 -0700

    Enhanced hexToInt and intToHex to handle more cases.

--- lib/src/ColorUtils.dart
+++ lib/src/ColorUtils.dart
@@ -11,30 +11,27 @@ class ColorUtils {
   static const String HEX_WHITE = '#FFFFFF';
 
   ///
-  /// Converts the given [hex] color string to the corresponding int
+  /// Converts the given [hex] color string to the corresponding int.
+  ///
+  /// Note that when no alpha/opacity is specified, 0xFF is assumed.
   ///
   static int hexToInt(String hex) {
-    if (hex.startsWith('#')) {
-      hex = hex.replaceFirst('#', 'FF');
-      return int.parse(hex, radix: 16);
-    } else {
-      if (hex.length == 6) {
-        hex = 'FF' + hex;
-      }
-      return int.parse(hex, radix: 16);
-    }
+    final hexDigits = hex.startsWith('#') ? hex.substring(1) : hex;
+    final hexMask = hexDigits.length <= 6 ? 0xFF000000 : 0;
+    final hexValue = int.parse(hexDigits, radix: 16);
+    assert(hexValue >= 0 && hexValue <= 0xFFFFFFFF);
+    return hexValue | hexMask;
   }
 
   ///
   /// Converts the given integer [i] to a hex string with a leading #.
   ///
+  /// Note that only the RGB values will be returned (like #RRGGBB), so
+  /// and alpha/opacity value will be stripped.
+  ///
   static String intToHex(int i) {
-    var s = i.toRadixString(16);
-    if (s.length == 8) {
-      return '#' + s.substring(2).toUpperCase();
-    } else {
-      return '#' + s.toUpperCase();
-    }
+    assert(i >= 0 && i <= 0xFFFFFFFF);
+    return '#${(i & 0xFFFFFF | 0x1000000).toRadixString(16).substring(1).toUpperCase()}';
   }
 
   ///
