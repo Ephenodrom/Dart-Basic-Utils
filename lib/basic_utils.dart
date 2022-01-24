d6121d6dd04189607731784da1d7fc41e60c1af5
RicardoRB <ricardoromebeni@gmail.com>
2022-01-24 08:26:51 +0100

    EnumUtils is not exported

--- lib/basic_utils.dart
+++ lib/basic_utils.dart
@@ -1,29 +1,45 @@
 library basic_utils;
 
+export 'package:pointycastle/api.dart' hide Padding;
+export 'package:pointycastle/asn1.dart';
+export 'package:pointycastle/asymmetric/api.dart';
+// Export other libraries
+export 'package:pointycastle/ecc/api.dart';
+
+export 'src/Asn1Utils.dart';
+export 'src/ColorUtils.dart';
+export 'src/CryptoUtils.dart';
+export 'src/DateUtils.dart';
+export 'src/DnsUtils.dart';
+
+/// Export util classes
+export 'src/DomainUtils.dart';
+export 'src/EmailUtils.dart';
+export 'src/EnumUtils.dart';
+export 'src/FunctionDefs.dart';
+export 'src/HttpUtils.dart';
+export 'src/IterableUtils.dart';
+export 'src/MathUtils.dart';
+export 'src/SortUtils.dart';
+export 'src/StringUtils.dart';
+export 'src/X509Utils.dart';
+
 /// Export model and other stuff
 export 'src/model/CountryCodeList.dart';
+export 'src/model/DnsApiProvider.dart';
 export 'src/model/Domain.dart';
 export 'src/model/EmailAddress.dart';
 export 'src/model/GtldList.dart';
-export 'src/model/IdnCountryCodeList.dart';
-export 'src/model/PublicSuffix.dart';
-export 'src/model/LengthUnits.dart';
-export 'src/model/exception/HttpResponseException.dart';
-export 'src/model/RRecordType.dart';
-export 'src/model/RRecord.dart';
-export 'src/model/ResolveResponse.dart';
 export 'src/model/HttpRequestReturnType.dart';
-export 'src/model/pkcs7/Pkcs7CertificateData.dart';
-export 'src/model/x509/X509CertificateData.dart';
-export 'src/model/x509/X509CertificateObject.dart';
-export 'src/model/x509/VmcData.dart';
-export 'src/model/x509/X509CertificateDataExtensions.dart';
-export 'src/model/x509/X509CertificateValidity.dart';
-export 'src/model/x509/ExtendedKeyUsage.dart';
+export 'src/model/IdnCountryCodeList.dart';
+export 'src/model/LengthUnits.dart';
+export 'src/model/PublicSuffix.dart';
+export 'src/model/RRecord.dart';
+export 'src/model/RRecordType.dart';
+export 'src/model/ResolveResponse.dart';
 export 'src/model/csr/CertificateSigningRequestData.dart';
 export 'src/model/csr/CertificateSigningRequestExtensions.dart';
-export 'src/model/x509/X509CertificatePublicKeyData.dart';
-export 'src/model/DnsApiProvider.dart';
+export 'src/model/exception/HttpResponseException.dart';
 
 /// OCSP
 export 'src/model/ocsp/BasicOCSPResponse.dart';
@@ -33,25 +49,11 @@ export 'src/model/ocsp/OCSPResponse.dart';
 export 'src/model/ocsp/OCSPResponseData.dart';
 export 'src/model/ocsp/OCSPResponseStatus.dart';
 export 'src/model/ocsp/OCSPSingleResponse.dart';
-
-/// Export util classes
-export 'src/DomainUtils.dart';
-export 'src/EmailUtils.dart';
-export 'src/StringUtils.dart';
-export 'src/MathUtils.dart';
-export 'src/HttpUtils.dart';
-export 'src/DnsUtils.dart';
-export 'src/SortUtils.dart';
-export 'src/ColorUtils.dart';
-export 'src/DateUtils.dart';
-export 'src/X509Utils.dart';
-export 'src/IterableUtils.dart';
-export 'src/CryptoUtils.dart';
-export 'src/Asn1Utils.dart';
-export 'src/FunctionDefs.dart';
-
-// Export other libraries
-export 'package:pointycastle/ecc/api.dart';
-export 'package:pointycastle/asymmetric/api.dart';
-export 'package:pointycastle/asn1.dart';
-export 'package:pointycastle/api.dart' hide Padding;
+export 'src/model/pkcs7/Pkcs7CertificateData.dart';
+export 'src/model/x509/ExtendedKeyUsage.dart';
+export 'src/model/x509/VmcData.dart';
+export 'src/model/x509/X509CertificateData.dart';
+export 'src/model/x509/X509CertificateDataExtensions.dart';
+export 'src/model/x509/X509CertificateObject.dart';
+export 'src/model/x509/X509CertificatePublicKeyData.dart';
+export 'src/model/x509/X509CertificateValidity.dart';
