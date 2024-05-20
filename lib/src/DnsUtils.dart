28f55fe04e6b59ffa943653e56b1994fa25db609
Noo6 <2746419228@qq.com>
2024-05-20 20:12:12 +0800

    Add 'dnsApiUrl' parameter to lookupRecord function in DnsUtils class

--- lib/src/DnsUtils.dart
+++ lib/src/DnsUtils.dart
@@ -19,14 +19,18 @@ class DnsUtils {
   ///
   static Future<List<RRecord>?> lookupRecord(String name, RRecordType type,
       {bool dnssec = false,
-      DnsApiProvider provider = DnsApiProvider.GOOGLE}) async {
+      DnsApiProvider provider = DnsApiProvider.GOOGLE,
+      String? dnsApiUrl}) async {
     var queryParameters = <String, dynamic>{};
     queryParameters.putIfAbsent('name', () => name);
     queryParameters.putIfAbsent('type', () => _getTypeFromType(type));
     queryParameters.putIfAbsent('dnssec', () => dnssec.toString());
 
-    assert(_dnsApiProviderUrl.length == DnsApiProvider.values.length);
-    var _baseUrl = _dnsApiProviderUrl[provider];
+    var _baseUrl = dnsApiUrl;
+    if (_baseUrl == null) {
+      assert(_dnsApiProviderUrl.length == DnsApiProvider.values.length);
+      _baseUrl = _dnsApiProviderUrl[provider];
+    }
 
     var headers = {'Accept': 'application/dns-json'};
 
